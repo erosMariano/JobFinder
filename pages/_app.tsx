f961dedb93f307b2433491f33f0e4aa6ffbfda7b
erosMariano <erosmariano1@gmail.com>
2022-03-19 11:13:47 -0300

    hotfix: favicion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,38 +5,7 @@ import { AppProps } from 'next/app'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="theme-color" content="#1a1a1a"/>
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet" />
-
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/icon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/icon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#1a1a1a" />
-      </Head>
+      
       <Component {...pageProps} />
     </>
   )
---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <meta name="theme-color" content="#1a1a1a" />
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet" />
+
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+                <meta name="description" content="Description" />
+                <meta name="keywords" content="Keywords" />
+
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/icons/icon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="/icons/icon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link rel="apple-touch-icon" href="/apple-icon.png" />
+                <meta name="theme-color" content="#1a1a1a" />
+
+                <link rel="icon" type="image/png" href="/icons/icon-512x512.png" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
